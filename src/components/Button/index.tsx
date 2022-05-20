8484fdc31fd74732724d8980bd8ae6d4c218e85c
Davi <davihgs20@gmail.com>
2022-05-20 14:20:45 -0300

    Adding button types and logic for hover

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from "react"
+import { theme } from "../../themes/theme"
 import { ContainerButton } from "./styles"
+
+export enum EButton {
+    'MainButton',
+    'AlertButton'
+}
 
 interface IProps {
     text: string
-    color: string
+    type: EButton
     onClick?: () => void
 }
 
 export const Button = (props: IProps) => {
+    const [color, setColor] = useState('');
+    const [hoverColor, setHoverColor] = useState('');
+    const [fontHoverColor, setFontHoverColor] = useState('');
+
+    useEffect(()=>{
+        if(props.type === 0){
+            setColor(theme.pallete.blue.main)
+            setHoverColor(theme.pallete.blue.second)
+            setFontHoverColor(theme.pallete.assistant.white)
+        }
+
+        if(props.type === 1){
+            setColor(theme.pallete.assistant.alertButton)   
+            setHoverColor(theme.pallete.status.fillRed)
+            setFontHoverColor(theme.pallete.assistant.black)
+        }
+    }, [props.type])
 
     return(
         <>
-            <ContainerButton color={props.color} onClick={props.onClick}>
+            <ContainerButton 
+                color={color} 
+                hoverColor={hoverColor} 
+                fontHoverColor={fontHoverColor} 
+                onClick={props.onClick}
+            >
                 {props.text}
             </ContainerButton>
         </>
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 interface IButtonProps {
     color: string
+    hoverColor: string
+    fontHoverColor: string
 }
 
 export const ContainerButton = styled.div<IButtonProps>`
@@ -9,14 +11,21 @@ export const ContainerButton = styled.div<IButtonProps>`
 
     height: fit-content;
     margin: 5%;
-    padding: 25px;
+    padding: 15px 20px;
     
     background: ${props => props.color};
+    border: 2px solid; 
     border-radius: ${props => props.theme.shape.borderRadius};
+    border-color: ${props => props.color};
 
     color: ${props => props.theme.pallete.assistant.white};
     font-family: ${props => props.theme.typography.fontFamily};
     font-size: ${props => props.theme.typography.buttonTextSize};
     font-weight: ${props => props.theme.typography.buttonTextWeight};
     text-align: center;
+
+    &:hover {
+        background: ${props => props.hoverColor};
+        color:  ${props => props.fontHoverColor}
+    }
 `
