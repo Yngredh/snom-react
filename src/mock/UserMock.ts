7313628c0ab6b778d7c1b906ea07726530a1755c
Davi <davihgs20@gmail.com>
2022-12-17 17:02:22 -0300

    Add login call with new user data and token acess logic

--- src/mock/UserMock.ts
+++ src/mock/UserMock.ts
@@ -1,37 +1,27 @@
-import { User } from "../interfaces/User";
+import { IUser } from "../interfaces/IUser";
 
-export const UserMockTypeOne : User = {
+export const UserMockTypeOne : IUser = {
     id: 2,
     name: "Davi Gonçalves",
-    type: "",
     email: "davihgs20@gmail.com",
     password: "",
     nickname: "",
-    level: 0,
-    isUserActive: false,
-    experience: 0,
     icon: "/img/profile/teste.jpg",
-    permission: {
-        id : 0,
-        description: ""
-    }
+    role: "Desenvolvedor de Software",
+    isAdministrator: false,
+    hasPermission: false
 }
 
-export const UserMockTypeTwo : User = {
+export const UserMockTypeTwo : IUser = {
     id: 2,
     name: "Yngredh Costa da Cruz",
-    type: "",
     email: "yngredh.cruz@gmail.com",
     password: "",
     nickname: "",
-    level: 0,
-    isUserActive: false,
-    experience: 0,
     icon: "https://github.com/Yngredh.png",
-    permission: {
-        id : 0,
-        description: ""
-    }
+    role: "Engenheira de Software",
+    isAdministrator: false,
+    hasPermission: false
 }
 
 export const ListedUsers = [UserMockTypeOne, UserMockTypeTwo, UserMockTypeOne, UserMockTypeTwo, UserMockTypeOne, UserMockTypeTwo, UserMockTypeOne, UserMockTypeTwo, UserMockTypeOne, UserMockTypeTwo];
