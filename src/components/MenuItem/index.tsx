1f7e71f1c5608688af0ee7eb3b8f6f7cfb71da70
Davi <davihgs20@gmail.com>
2023-02-23 22:11:47 -0300

    Fixing menu item to be activade when the route is selected

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,15 +1,20 @@
+import { theme } from '../../themes/theme';
 import { Icon, Title, Container} from './styles';
 
 type Props = {
     title: string;
     icon: string;
+    isActive: boolean
 }
 
-const MenuItem = ({title, icon}: Props) => {
+const MenuItem = ({title, icon, isActive}: Props) => {
+    const iconActiveStyle : React.CSSProperties = 
+        isActive ? {filter: "invert(66%) sepia(86%) saturate(2248%) hue-rotate(198deg) brightness(99%) contrast(85%)"} : {};
+    const titleActiveStyle: React.CSSProperties = isActive ? {color: theme.pallete.blueViolet.light} : {};
     return(    
         <Container>
-            <Icon className="icon-menu" src={icon}></Icon>
-            <Title className="title-menu">{title}</Title>
+            <Icon style={iconActiveStyle} className="icon-menu" src={icon}></Icon>
+            <Title style={titleActiveStyle} className="title-menu">{title}</Title>
         </Container>
     );
 }
