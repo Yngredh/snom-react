1ea0b498ded611ea6e70450f4e759ffda4cd5f48
Davi <davihgs20@gmail.com>
2023-05-03 20:43:22 -0300

    Add logout option, permissions fix and menu logo

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -4,15 +4,16 @@ import { Icon, Title, Container} from './styles';
 type Props = {
     title: string;
     icon: string;
-    isActive: boolean
+    isActive: boolean;
+    onClick?: () => void
 }
 
-const MenuItem = ({title, icon, isActive}: Props) => {
+const MenuItem = ({title, icon, isActive, onClick}: Props) => {
     const iconActiveStyle : React.CSSProperties = 
         isActive ? {filter: "invert(66%) sepia(86%) saturate(2248%) hue-rotate(198deg) brightness(99%) contrast(85%)"} : {};
     const titleActiveStyle: React.CSSProperties = isActive ? {color: theme.pallete.blueViolet.light} : {};
     return(    
-        <Container>
+        <Container onClick={onClick}>
             <Icon style={iconActiveStyle} className="icon-menu" src={icon}></Icon>
             <Title style={titleActiveStyle} className="title-menu">{title}</Title>
         </Container>
