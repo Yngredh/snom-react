a3afc1e302e86e82f414895ee538e6fdb65cafe7
Yngredh <yngredh.costa@gmail.com>
2023-06-14 21:39:55 -0300

    scroll bar XD

--- src/components/ModuleManagementList/styles.ts
+++ src/components/ModuleManagementList/styles.ts
@@ -6,6 +6,24 @@ export const ModuleList = styled.div`
     width: 92%;
     height: 80%;
     margin: 4%;
+
+    overflow-y: auto;
+
+    ::-webkit-scrollbar{
+    width : 8px;
+    }
+    ::-webkit-scrollbar-track{
+        box-shadow: inset 0 0 5px ${props => props.theme.pallete.cyanGreen.dark}; 
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb{
+        background: ${props => props.theme.pallete.cyanGreen.dark};
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-thumb:hover{
+        background: ${props => props.theme.pallete.cyanGreen.dark};
+    }
 `
 export const Module = styled.div`
     display: flex;
